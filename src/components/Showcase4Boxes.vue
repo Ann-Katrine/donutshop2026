08d6e44232e705c83b0126511c713792c7b6f420
Ann-Katrine Nykjær <33825997+Ann-Katrine@users.noreply.github.com>
2026-05-28 17:18:59 +0200

    made a new component Showcase4Boxes
    I have made the new component Showcase4Boxes, so i can use it on the HomeView to shocase 4 donut and a title to. as i will be have a couple of them.
    
    no issue

---
+++ src/components/Showcase4Boxes.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex justify-center">
+    <div>
+      <h2>{{ title }}</h2>
+      <div class="flex flex-row gap-x-24">
+        <img v-for="box in boxes" :key="box.image" :src="box.image" :alt="box.title" height="400" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Showcase4BoxesProps {
+  title: string
+  boxes: Box[]
+}
+
+interface Box {
+  image: string
+  title: string
+  description: string
+}
+
+defineProps<Showcase4BoxesProps>()
+</script>
