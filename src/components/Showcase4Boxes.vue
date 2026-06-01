1921de6e1e072b26fc5381aa4c3cbcc49045d661
Ann-Katrine Nykjær <33825997+Ann-Katrine@users.noreply.github.com>
2026-06-01 23:05:05 +0200

    lint and prettier format
    It is only a format change
    
    no issue

--- src/components/Showcase4Boxes.vue
+++ src/components/Showcase4Boxes.vue
@@ -3,7 +3,14 @@
     <div>
       <h2>{{ title }}</h2>
       <div class="flex flex-row gap-x-24">
-        <img v-for="box in boxes" :key="box.image" :src="box.image" :alt="box.title" height="400" />
+        <img
+          v-for="box in boxes"
+          :key="box.image"
+          :src="box.image"
+          :alt="box.title"
+          height="50"
+          width="350"
+        />
       </div>
     </div>
   </div>
